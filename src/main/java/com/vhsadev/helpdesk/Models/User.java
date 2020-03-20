7e47e22d53dc7e74a11a303cf51e54586223ee51
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-03-20 11:40:10 -0300

    Configuração do banco de dados e criação do model de Usuário.

---
+++ src/main/java/com/vhsadev/helpdesk/Models/User.java
@@ -0,0 +1,111 @@
+package com.vhsadev.helpdesk.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message = "The field EMAIL is required.")
+	@Email(message = "Please, provide a valid e-mail address.")
+	private String email;
+	
+	@NotEmpty(message = "The field NAME is required.")
+	private String name;
+	
+	@NotEmpty(message = "The field LAST NAME is required.")
+	private String lastName;
+	
+	@NotEmpty(message = "The field PASSWORD is required.")
+	@Length(min = 6, max = 32,message = "You must provide a password with a minimum of 6 and a maximum of 32 characters.")
+	private String password;
+	
+	private Boolean isActive;
+	
+	public User(){}
+	
+	
+
+	public User(Long id, String email, String name, String lastName, String password, Boolean isActive) {
+		this.id = id;
+		this.email = email;
+		this.name = name;
+		this.lastName = lastName;
+		this.password = password;
+		this.isActive = isActive;
+	}
+
+	public User(String email, String name, String lastName, String password, Boolean isActive) {
+		this.email = email;
+		this.name = name;
+		this.lastName = lastName;
+		this.password = password;
+		this.isActive = isActive;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+	
+}

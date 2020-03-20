dc597084e7e97f845feb5b03910be8f4135b8b26
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-03-20 11:16:32 -0300

    Commit Inicial

---
+++ src/main/java/com/vhsadev/helpdesk/HelpDeskSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.vhsadev.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskSpringMvcApplication.class, args);
+	}
+
+}
